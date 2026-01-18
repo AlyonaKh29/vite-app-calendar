7364f9585c4f0e4fe6989af4e7e64006b7d674ba
Alyona <alena.bentley.aston@gmail.com>
2026-01-18 03:04:37 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Calendar } from './components/Calendar/Calendar'
+//import './App.css'
+
+function App() {
+  const now = new Date(2017, 2, 8);
+
+  return (
+    <Calendar date={now} />
+  )
+}
+
+export default App
